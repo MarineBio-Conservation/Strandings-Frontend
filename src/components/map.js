b4cc712106c30812617bdb57557f7c121b0a6723
John Clark <john@marinebio.org>
2021-06-16 18:37:33 +0000

    Capture map component in state variable.

--- src/components/map.js
+++ src/components/map.js
@@ -1,74 +1,67 @@
-import { memo } from "react";
+import React from "react";
 import {
   GoogleMap,
   useJsApiLoader,
-  MarkerClusterer,
   Marker,
+  MarkerClusterer,
 } from "@react-google-maps/api";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-function StrandingsMap({ strandings }) {
-//  const [map, setMap] = useState(null);
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = { lat: 54.2307982, lng: -2.847663 };
 
-  const options = {
-    imagePath: process.env.PUBLIC_URL + "/images/",
-  };
+function StrandingsMap({ strandings, boundsUpdated }) {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBVRxbVl5Qtm2LFX8ZvYZ34Wg8R_iIRV84",
+  });
 
-  const mapContainerStyle = {
-    height: "100vh",
-    width: "100vw",
-  };
+  const [map, setMap] = React.useState(null);
 
-  const center = { lat: 54.2307982, lng: -2.847663 };
+  const onLoad = React.useCallback(function callback(map) {
+    setMap(map);
+  }, []);
 
-  const onBoundsChanged = () => {
- //   if (map) {
-   //   console.log(map.getBounds());
-   //}
-  };
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+  
+  const onBoundsChange = React.useCallback(function callback() {
+    if (map) {
+			console.log(map.getBounds());
+			if (boundsUpdated) {
+				boundsUpdated(map.getBounds());
+			}
+		}
+  }, [map, boundsUpdated]);
 
-//  const onLoad = useCallback(function callback(map) {
-//    setMap(map);
-//  }, []);
-
-  const StrandingsMap = () => {
-    const { isLoaded, loadError } = useJsApiLoader({
-      googleMapsApiKey: "AIzaSyBVRxbVl5Qtm2LFX8ZvYZ34Wg8R_iIRV84",
-    });
-
-    return !isLoaded ? (
-      <Loader type="Puff" color="#00BFFF" height={80} width={80} />
-    ) : loadError ? (
-      <div>Error loading map. Please refresh.</div>
-    ) : (
-      <>
-        <GoogleMap
-          id="strandings-map"
-          mapContainerStyle={mapContainerStyle}
-          zoom={6}
-          center={center}
-          gridSize={30}
-          onBoundsChanged={onBoundsChanged}
-          // onLoad={onLoad}
-          // onUnmount={onUnmount}
-        >
-          <MarkerClusterer options={options}>
-            {(clusterer) =>
-              strandings.map((location) => (
-                <Marker
-                  key={location.id}
-                  position={location.position}
-                  clusterer={clusterer}
-                />
-              ))
-            }
-          </MarkerClusterer>
-        </GoogleMap>
-      </>
-    );
-  };
-  return <StrandingsMap />;
+  const options = { imagePath: process.env.PUBLIC_URL + "/images/" };
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={5}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+	  onBoundsChange={onBoundsChange}
+    >
+      <MarkerClusterer options={options}>
+        {(clusterer) =>
+          strandings.map((location) => (
+            <Marker
+              key={location.id}
+              position={location.position}
+              clusterer={clusterer}
+            />
+          ))
+        }
+      </MarkerClusterer>
+    </GoogleMap>
+  ) : (
+    <>Loading...</>
+  );
 }
 
-export default memo(StrandingsMap);
+export default React.memo(StrandingsMap);
