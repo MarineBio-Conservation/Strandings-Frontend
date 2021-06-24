a49c4281588a5c4debaa73ca83cc27ae7bf1fd3b
John Clark <john@marinebio.org>
2021-06-24 17:47:01 +0000

    Simplified table

--- src/components/table.js
+++ src/components/table.js
@@ -1,68 +1,33 @@
 function StrandingsTable({ strandings }) {
   const rows = strandings.map((stranding) => {
     return (
-      <tr key={stranding.id}>
-        <td
-          className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6"
-          key={stranding.id + "date"}
-        >
-          <div className="flex items-center">
-            <div
-              className="flex-shrink-0 w-10 h-10 hidden 
-                  sm:table-cell"
-            ></div>
-            <div className="ml-3">
-              <p className="text-gray-900 whitespace-no-wrap">
-                {new Date(stranding.date).toDateString()}
-              </p>
-            </div>
-          </div>
-        </td>
-        <td
-          className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6"
-          key={stranding.id + "region"}
-        >
-          <p className="text-gray-900 whitespace-no-wrap text-center capitalize">
-            {stranding.region
-              ? stranding.region
-              : "Lat:" +
-                stranding.position.lat.toFixed(1) +
-                " Lng: " +
-                stranding.position.lng.toFixed(1)}
-          </p>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6">
-          <p className="text-gray-900 whitespace-no-wrap text-center capitalize">
-            {stranding.animal_type}
-          </p>
-        </td>
-        <td
-          className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6"
-          key={stranding.id + "died"}
-        >
-          <p className="text-gray-900 whitespace-no-wrap text-center">
-            {stranding.died}
-          </p>
-        </td>
-        <td
-          className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6"
-          key={stranding.id + "invest"}
-        >
-          <p className="text-gray-900 whitespace-no-wrap text-center capitalize">
-            {stranding.investigation_type
-              ? stranding.investigation_type
-              : "Unknown"}
-          </p>
-        </td>
-        <td
-          className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-1/6"
-          key={stranding.id + "cause"}
-        >
-          <p className="text-gray-900 whitespace-no-wrap text-center capitalize">
-            {stranding.causes ? stranding.causes : "Unknown"}
-          </p>
-        </td>
-      </tr>
+      <>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap">
+          {new Date(stranding.date).toDateString()}
+        </p>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center capitalize">
+          {stranding.region
+            ? stranding.region
+            : "Lat:" +
+              stranding.position.lat.toFixed(1) +
+              " Lng: " +
+              stranding.position.lng.toFixed(1)}
+        </p>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center capitalize">
+          {stranding.animal_type}
+        </p>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center">
+          {stranding.died}
+        </p>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center capitalize">
+          {stranding.investigation_type
+            ? stranding.investigation_type
+            : "Unknown"}
+        </p>
+        <p className="px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center capitalize">
+          {stranding.causes ? stranding.causes : "Unknown"}
+        </p>
+        </>
     );
   });
 
@@ -72,61 +37,57 @@ function StrandingsTable({ strandings }) {
         <div className="py-8">
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-              <table className="min-w-full leading-normal">
-                <thead>
-                  <tr>
-                    <th
+              <div className="min-w-full leading-normal grid grid-cols-6">
+                    <div
                       className="px-5 py-3 border-b-2 border-gray-200 
                             bg-gray-100 text-center text-xs 
                             font-semibold text-gray-600 uppercase 
-                            tracking-wider w-1/6"
+                            tracking-wider"
                     >
                       Event Date
-                    </th>
-                    <th
+                    </div>
+                    <div
                       className="px-5 py-3 border-b-2 
                             border-gray-200 bg-gray-100 text-center 
                             text-xs font-semibold text-gray-600 
-                            uppercase tracking-wider w-1/6"
+                            uppercase tracking-wider"
                     >
                       Region
-                    </th>
-                    <th
+                    </div>
+                    <div
                       className="px-5 py-3 border-b-2 
                             border-gray-200 bg-gray-100 text-center 
                             text-xs font-semibold text-gray-600 
-                            uppercase tracking-wider w-1/6"
+                            uppercase tracking-wider"
                     >
                       Stranded
-                    </th>
-                    <th
+                    </div>
+                    <div
                       className="px-5 py-3 border-b-2 
                             border-gray-200 bg-gray-100 text-center 
                             text-xs font-semibold text-gray-600 
-                            uppercase tracking-wider w-1/6"
+                            uppercase tracking-wider"
                     >
                       Died
-                    </th>
-                    <th
+                    </div>
+                    <div
                       className="px-5 py-3 border-b-2 
                             border-gray-200 bg-gray-100 text-center 
                             text-xs font-semibold text-gray-600 
-                            uppercase tracking-wider w-1/6"
+                            uppercase tracking-wider"
                     >
                       Investigation
-                    </th>
-                    <th
+                    </div>
+                    <div
                       className="px-5 py-3 border-b-2 
                             border-gray-200 bg-gray-100 text-center 
                             text-xs font-semibold text-gray-600 
-                            uppercase tracking-wider w-1/6"
+                            uppercase tracking-wider"
                     >
                       Cause
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-              </table>
+                    </div>
+                {rows}
+              </div>
             </div>
           </div>
         </div>
