4b7e8fe59eaf74912c08378a450dec412814eb79
John Clark <john@marinebio.org>
2021-07-25 21:48:57 +0000

    make logo clickabke

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -35,18 +35,20 @@ export default function Navbar() {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src={logo}
-                    alt="MarineBio Strandings"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src={logo}
-                    alt="MarineBio Strandings"
-                  />
-                </div>
+                <Link to="/">
+                  <div className="flex-shrink-0 flex items-center">
+                    <img
+                      className="block lg:hidden h-8 w-auto"
+                      src={logo}
+                      alt="MarineBio Strandings"
+                    />
+                    <img
+                      className="hidden lg:block h-8 w-auto"
+                      src={logo}
+                      alt="MarineBio Strandings"
+                    />
+                  </div>
+                </Link>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
