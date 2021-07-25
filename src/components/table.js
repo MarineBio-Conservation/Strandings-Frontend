b4415d65f44d393d3feb12ee3a99db3d8f9ff888
John Clark <john@marinebio.org>
2021-07-25 19:03:32 +0000

    Make the main table a table to improve accessibility

--- src/components/table.js
+++ src/components/table.js
@@ -8,65 +8,52 @@ function StrandingsTable() {
 
   const dataStyle =
     "px-5 py-5 border-b border-gray-200 text-sm text-gray-900 whitespace-no-wrap text-center capitalize";
-  const dataStyleHiddenMobile = dataStyle + " hidden md:block";
+  const dataStyleHiddenMobile = dataStyle + " hidden md:table-cell";
 
   const headingStyle =
     "px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider";
-  const headingStyleHiddenMobile = headingStyle + " hidden md:block";
+  const headingStyleHiddenMobile = headingStyle + " hidden md:table-cell";
 
   const rows = strandings.map((stranding) => {
     return (
-      <React.Fragment key={stranding.id}>
-        <p role="cell" className={dataStyle} key={stranding.id + "_date"}>
+      <tr key={stranding.id}>
+        <td className={dataStyle} key={stranding.id + "_date"}>
           {new Date(stranding.date).toDateString()}
-        </p>
-        <p
-          role="cell"
-          className={dataStyleHiddenMobile}
-          key={stranding.id + "_pos"}
-        >
+        </td>
+        <td className={dataStyleHiddenMobile} key={stranding.id + "_pos"}>
           {stranding.region
             ? stranding.region
             : "Lat:" +
               stranding.position.lat.toFixed(1) +
               " Lng: " +
               stranding.position.lng.toFixed(1)}
-        </p>
-        <p role="cell" className={dataStyle} key={stranding.id + "_type"}>
+        </td>
+        <td className={dataStyle} key={stranding.id + "_type"}>
           {stranding.animal_type}
-        </p>
-        <p role="cell" className={dataStyle} key={stranding.id + "_died"}>
+        </td>
+        <td className={dataStyle} key={stranding.id + "_died"}>
           {stranding.died}
-        </p>
-        <p
-          role="cell"
+        </td>
+        <td
           className={dataStyleHiddenMobile}
           key={stranding.id + "_investigation"}
         >
           {stranding.investigation_type
             ? stranding.investigation_type
             : "Unknown"}
-        </p>
-        <p
-          role="cell"
-          className={dataStyleHiddenMobile}
-          key={stranding.id + "_cause"}
-        >
+        </td>
+        <td className={dataStyleHiddenMobile} key={stranding.id + "_cause"}>
           {stranding.causes ? stranding.causes : "Unknown"}
-        </p>
-        <p
-          role="cell"
-          className={dataStyleHiddenMobile}
-          key={stranding.id + "_details"}
-        >
+        </td>
+        <td className={dataStyleHiddenMobile} key={stranding.id + "_details"}>
           <Link
             key={stranding.id + "_detailsLink"}
             to={"/view?id=" + stranding.id}
           >
             View...
           </Link>
-        </p>
-      </React.Fragment>
+        </td>
+      </tr>
     );
   });
 
@@ -76,33 +63,20 @@ function StrandingsTable() {
         <div className="py-8">
           <div className="-mx-4 md:-mx-8 px-4 md:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-              <div
-                className="min-w-full leading-normal grid grid-cols-3 md:grid-cols-7"
-                role="table"
-              >
-                <div role="columnheader" className={headingStyle}>
-                  Date
-                </div>
-                <div role="columnheader" className={headingStyleHiddenMobile}>
-                  Region
-                </div>
-                <div role="columnheader" className={headingStyle}>
-                  Animal
-                </div>
-                <div role="columnheader" className={headingStyle}>
-                  Died
-                </div>
-                <div role="columnheader" className={headingStyleHiddenMobile}>
-                  Investigation
-                </div>
-                <div role="columnheader" className={headingStyleHiddenMobile}>
-                  Cause
-                </div>
-                <div role="columnheader" className={headingStyleHiddenMobile}>
-                  Details
-                </div>
-                {rows}
-              </div>
+              <table className="min-w-full leading-normal table-auto">
+                <thead>
+                  <tr>
+                    <th className={headingStyle}>Date</th>
+                    <th className={headingStyleHiddenMobile}>Region</th>
+                    <th className={headingStyle}>Animal</th>
+                    <th className={headingStyle}>Died</th>
+                    <th className={headingStyleHiddenMobile}>Investigation</th>
+                    <th className={headingStyleHiddenMobile}>Cause</th>
+                    <th className={headingStyleHiddenMobile}>Details</th>
+                  </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+              </table>
             </div>
           </div>
         </div>
