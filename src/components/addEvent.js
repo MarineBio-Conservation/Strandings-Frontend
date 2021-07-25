eec178fb3721e9aad7f757e4fcd484b822989194
John Clark <john@marinebio.org>
2021-07-25 13:37:27 +0000

    Form updates.

--- src/components/addEvent.js
+++ src/components/addEvent.js
@@ -2,30 +2,36 @@ import React from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import "react-day-picker/lib/style.css";
 import AddEventMap from "./addEventMap";
+import { useForm } from "react-hook-form";
+
+const labelStyle =
+  "block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4";
+const inputTextBoxStyle =
+  "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline";
+const checkBoxStyle =
+  "appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2";
+const submitStyle =
+  "m-auto shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded";
 
 function AddEvent() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div className="flex justify-center p-8">
-      <form className="w-full container">
+      <form className="w-full container" onSubmit={handleSubmit(onSubmit)}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="date"
-            >
+            <label className={labelStyle} htmlFor="date">
               Date
             </label>
           </div>
           <div className="md:w-5/6">
-            <DayPickerInput inputProps={{ id: "date" }} />
+            <DayPickerInput inputProps={{ id: "date" }} ref={register} />
           </div>
         </div>
         <div className="md:items-center mb-6">
           <div className="w-full">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="inline-password"
-            >
+            <label className={labelStyle} htmlFor="inline-password">
               Location
             </label>
           </div>
@@ -35,103 +41,143 @@ function AddEvent() {
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="animals"
-            >
+            <label className={labelStyle} htmlFor="animals">
               Animals Stranded
             </label>
           </div>
           <div className="md:w-5/6">
-            <textarea
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="animals"
-              rows="5"
-            />
+            <textarea className={inputTextBoxStyle} id="animals" rows="5" ref={register} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="died"
-            >
+            <label className={labelStyle} htmlFor="died">
               Died
             </label>
           </div>
           <div className="md:w-5/6">
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="died"
-              type="number"
-            />
+            <input className={inputTextBoxStyle} id="died" type="number" ref={register} />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="investigation"
-            >
+            <label className={labelStyle} htmlFor="investigation">
               Investigation Details
             </label>
           </div>
           <div className="md:w-5/6">
             <textarea
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className={inputTextBoxStyle}
               id="investigation"
               rows="5"
+              ref={register}
             />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="investigation_results"
-            >
+            <label className={labelStyle} htmlFor="investigation_results">
               Investigation Results
             </label>
           </div>
           <div className="md:w-5/6">
-            <fieldset
-              id="investigation_results"
-		  	  className="flex flex-wrap"
-            >
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Unknown" />Unknown</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Pending" />Pending</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Disease" />Disease</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Pollution" />Pollution</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Vessel Strike" />Vessel Strike</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Bycatch" />Bycatch</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Entanglement" />Entanglement</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Sonar" />Sonar</label>
-              <label className="px-3"><input className="appearance-none checked:bg-blue-600 checked:border-transparent rounded mx-2" type="checkbox" value="Other" />Other</label>
+            <fieldset id="investigation_results" className="flex flex-wrap">
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Unknown"
+                  ref={register}
+                />
+                Unknown
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Pending"
+                  ref={register}
+                />
+                Pending
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Disease"
+                  ref={register}
+                />
+                Disease
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Pollution"
+                  ref={register}
+                />
+                Pollution
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Vessel Strike"
+                  ref={register}
+                />
+                Vessel Strike
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Bycatch"
+                  ref={register}
+                />
+                Bycatch
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Entanglement"
+                  ref={register}
+                />
+                Entanglement
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Sonar"
+                  ref={register}
+                />
+                Sonar
+              </label>
+              <label className="px-3">
+                <input
+                  className={checkBoxStyle}
+                  type="checkbox"
+                  value="Other"
+                  ref={register}
+                />
+                Other
+              </label>
             </fieldset>
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
-            <label
-              className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
-              htmlFor="references"
-            >
+            <label className={labelStyle} htmlFor="references">
               References
             </label>
           </div>
           <div className="md:w-5/6">
-            <textarea
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="references"
-              rows="5"
-            />
+            <textarea className={inputTextBoxStyle} id="references" rows="5" ref={register}/>
           </div>
         </div>
         <div className="flex">
-          <button
-            className="m-auto shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
+          <button className={submitStyle} type="button">
             Submit
           </button>
         </div>
